575511d1e68b327caeb643ba67e1f293f7b60b6e
Francis <francisalexander000@gmail.com>
2022-10-19 06:39:26 -0700

    updated tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,6 @@
 
 /**
  * print_alphabet_x10 - prints alphabet 10 times
- *
  * Return:void
  */
 
@@ -10,17 +9,30 @@ void print_alphabet_x10(void)
 {
 	char c;
 
+
+
 	int i = 0;
+	
 	while (i < 10)
+	
 	{
+	
 		c = 'a';
+	
 		while (c <= 'z')
+	
 		{
+	
 			_putchar(c);
+	
 			c++;
+	
 		}
+	
 		_putchar('\n');
+	
 		i++;
+	
 	}
 
 }
